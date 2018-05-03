3506c836051c06669425c4cee29d8eb51ad460bc
wangxiaola <1015559188@qq.com>
2018-05-03 10:15:41 +0800

    渠道

---
+++ channelTool/channelTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  channelTool
+//
+//  Created by 王小腊 on 2018/5/3.
+//  Copyright © 2018年 王小腊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
